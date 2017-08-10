fefa74125b93993b6cad01a3a94a5e1e6091abe4
Arel English <arelenglish@gmail.com>
2017-08-10 17:32:52 -0400

    add bootstrap and a user model so people can express interest in making their own map

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: :create
   root to: 'pages#index'
   # resources :flights
   # resources :locations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731143837) do
+ActiveRecord::Schema.define(version: 20170810203816) do
 
   create_table "flights", force: :cascade do |t|
     t.datetime "flight_date"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 20170731143837) do
     t.string "usage_type"
     t.string "power"
     t.string "associated_airport"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
