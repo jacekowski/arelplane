f1b4a960d0cb6909917330e82a6f38dd1bab93ba
Arel English <arelenglish@gmail.com>
2017-08-10 01:15:46 -0400

    change map type

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
   for (var i = 0; i < trips.length; i++) {
     if (trips[i].length === 2) {
-      var polyline = L.polyline([trips[i][0], trips[i][1]], {color:'#10FFFF'}).addTo(mymap);
+      var polyline = L.polyline([trips[i][0], trips[i][1]], {color:'#1E90FF'}).addTo(mymap);
     }
   }
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
   // var polyline = L.polyline([[51.5, -0.09],[51.6, -0.09]], {color: '#00FFFF'}).addTo(mymap);
 
   // MapBox Dark Map (Costs money after 50,000 impressions)
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         accessToken: 'pk.eyJ1IjoiYXJlbGVuZ2xpc2giLCJhIjoiY2l6ZzNrNHZ3MDB1cDMzb3dqdmh3emhjbSJ9.1WoDWsWNnIg-Wq8LPf1j-A'
