da3f0e30fc44915784f18502c63b61edb81506c8
Arel English <arelenglish@gmail.com>
2017-09-05 22:01:33 -0400

    close #13. message now displayed while logbook uploading

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $("#upload-logbook-btn").click(function(){
+    $( "div.user-map" ).replaceWith( `
+      <div class="row align-items-center" style="height: 100%;">
+        <div class="col-12">
+          <div class="card border-info mb-3 mx-auto" style="max-width: 40rem;">
+            <div class="card-header"><h5>Logbook Uploading ...</h5></div>
+            <div class="card-body">
+              <p class="card-text">Please stay on this page while your logbook uploads.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    ` );
+  });
+
+  $('input:file').on("change", function() {
+    $('input:submit').prop('disabled', !$(this).val());
+  });
+});
