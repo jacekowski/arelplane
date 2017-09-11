e849aa02695d1a4e6b6cffd7eb51928ae0e33f73
Arel English <arelenglish@gmail.com>
2017-09-11 19:25:10 -0400

    speed things up by removing a loop

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,30 +8,28 @@ class User < ApplicationRecord
 
   has_many :flights
 
-  def flight_metrics
-    metrics = {}
-    top_location = Hash.new(0)
-    regions = []
-    aircraft = Hash.new(0)
-    num_locations = []
-    flights.all.each do |flight|
-      top_location[flight.to] += 1
-      top_location[flight.from] += 1
-      regions << flight.to.iso_region
-      regions << flight.from.iso_region
-      aircraft[flight.aircraft_id] += 1
-      num_locations << flight.to
-      num_locations << flight.from
-    end
-    metrics[:top_location] = top_location.key(top_location.values.max)
-    metrics[:num_regions] = regions.uniq.count
-    metrics[:top_aircraft] = aircraft.key(aircraft.values.max)
-    metrics[:num_locations] = num_locations.uniq.count
-    metrics
+  def top_location
+    Location.find(flights.pluck(:from_id, :to_id).flatten.group_by{|i| i}.max{|x,y| x[1].length <=> y[1].length}[0]).identifier
+  end
+
+  def top_region
+    Location.find(flights.pluck(:from_id, :to_id).flatten.group_by{|i| i}.max{|x,y| x[1].length <=> y[1].length}[0]).iso_region
+  end
+
+  def top_aircraft
+    flights.group(:aircraft_id).order('count_id DESC').limit(1).count(:id).keys.first
   end
 
   def total_flight_hours
     flights.pluck(:total_time).compact.sum.round(1)
+  end
+
+  def num_airports
+    flights.pluck(:from_id, :to_id).flatten.uniq.count
+  end
+
+  def num_regions
+    Location.find(flights.pluck(:from_id, :to_id).flatten.uniq).pluck(:iso_region).uniq.count
   end
 
 end
