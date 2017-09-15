360070e8464fb39d9751b76055500a3129779208
Arel English <arelenglish@gmail.com>
2017-09-15 11:28:20 -0400

    still working on it

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
   $('#add_waypoint_select').click(function (event) {
     var field = $("div.waypoint_fields:last").clone();
     field.addClass("mt-2")
+    var name = field.find('select').attr("name");
+    var fieldIndex = JSON.parse(name.match(/[\[]\d[\]]/)[0])[0]
+    fieldIndex++
+    var newName = name.replace(/[\[]\d[\]]/g, "["+fieldIndex+"]")
+    field.find('select').attr("name").replace(/[\[]\d[\]]/g, "["+fieldIndex+"]")
     // increment select[name] = flight[waypoints_attributes][0][location_id]
     // then need to solve the problem of the fields not being clickable
     field.insertAfter("div.waypoint_fields:last");
