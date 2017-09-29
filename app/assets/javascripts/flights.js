803aa68966be424ef673847a4ea48368f00af8f9
Arel English <arelenglish@gmail.com>
2017-09-29 19:20:27 -0400

    can create multiple waypoint fields, but can't click on new fields

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -1,6 +1,3 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
 $(document).ready(function() {
   $('#add_waypoint_select').click(function (event) {
     var field = $("div.waypoint_fields:last").clone();
@@ -9,8 +6,7 @@ $(document).ready(function() {
     var fieldIndex = JSON.parse(name.match(/[\[]\d[\]]/)[0])[0]
     fieldIndex++
     var newName = name.replace(/[\[]\d[\]]/g, "["+fieldIndex+"]")
-    field.find('select').attr("name").replace(/[\[]\d[\]]/g, "["+fieldIndex+"]")
-    // increment select[name] = flight[waypoints_attributes][0][location_id]
+    field.find('select').attr('name', newName);
     // then need to solve the problem of the fields not being clickable
     field.insertAfter("div.waypoint_fields:last");
     event.preventDefault(); // Prevent link from following its href
