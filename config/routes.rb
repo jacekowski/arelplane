0297bd088140f58376833181341ddbc9acc607c0
Arel English <arelenglish@gmail.com>
2017-10-04 22:02:21 -0400

    close #57

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
   post 'flight_search', to: 'flights#search'
   get 'flight_search', to: 'flights#search'
   resources :waypoints, only: :destroy
-  resources :locations
 
   devise_for :users,
     controllers: {
