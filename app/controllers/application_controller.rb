ecd4d771ef7723c08a4bba378c44cab569b419dd
Arel English <arelenglish@gmail.com>
2017-11-25 23:36:05 -0500

    close #69 user is redirected back to whatever page they were on after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_user_location!, if: :storable_location?
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+private
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
