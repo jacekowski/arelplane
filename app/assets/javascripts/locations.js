b26ce9fc974506935ca174608321d67ffc8fbe92
Arel English <arelenglish@gmail.com>
2018-04-04 16:13:32 -0400

    home base picker now shows saved home base

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     fetchExistingValues();
   } else if (pathname.includes("/@") && pathname.includes("/edit")) {
     loadData();
-
+    fetchHomeBase();
   }
 });
 
@@ -120,63 +120,27 @@ function fetchExistingValues() {
   });
 }
 
+function username() {
+  var pathname = window.location.pathname;
+  var match = pathname.split("/")[1];
+  return match;
+};
+
 function fetchHomeBase() {
   var homeBase = $('#home_base');
-  var arrivalSelect = $('#arrival_airport');
   $.ajax({
       type: 'GET',
-      url: '/api/v1/flights/' + flightID()
+      url: '/api/v1/users/' + username()
   }).then(function (data) {
-    var waypoints = data["waypoints"];
-    for (var i = 0; i < waypoints.length; i++) {
-      var waypointSelect = $('.waypoint-' + i)
-      $.ajax({
-        type: 'GET',
-        index: i,
-        url: '/api/v1/locations/' + waypoints[i].location_id
-      }).then(function (data) {
-        var waypointSelect = $('.waypoint-' + this.index);
-        // create the option and append to Select2
-        var option = new Option(data.identifier + " ("+ data.name + ")", data.id, true, true);
-        waypointSelect.append(option).trigger('change');
-        // manually trigger the `select2:select` event
-        waypointSelect.trigger({
-            type: 'select2:select',
-            params: {
-                data: data
-            }
-        });
-      });
-    };
-    $.ajax({
-      type: 'GET',
-      url: '/api/v1/locations/' + data.from_id
-    }).then(function (data){
-      // create the option and append to Select2
-      var option = new Option(data.identifier + " ("+ data.name + ")", data.id, true, true);
-      departureSelect.append(option).trigger('change');
-      // manually trigger the `select2:select` event
-      departureSelect.trigger({
-          type: 'select2:select',
-          params: {
-              data: data
-          }
-      });
-    });
-    $.ajax({
-      type: 'GET',
-      url: '/api/v1/locations/' + data.to_id
-    }).then(function (data){
-      // create the option and append to Select2
-      var option = new Option(data.identifier + " ("+ data.name + ")", data.id, true, true);
-      arrivalSelect.append(option).trigger('change');
-      // manually trigger the `select2:select` event
-      arrivalSelect.trigger({
-          type: 'select2:select',
-          params: {
-              data: data
-          }
-      });
+    // create the option and append to Select2
+    var option = new Option(data.home_base.identifier + " ("+ data.home_base.name + ")", data.home_base.id, true, true);
+    homeBase.append(option).trigger('change');
+    // manually trigger the `select2:select` event
+    homeBase.trigger({
+        type: 'select2:select',
+        params: {
+            data: data
+        }
     });
   });
 }
