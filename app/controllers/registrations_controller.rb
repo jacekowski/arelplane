2ec5ae45e81ed5578ae11abc0880f006344bc757
Arel English <arelenglish@gmail.com>
2018-04-09 13:08:22 -0400

    user no longer needs to give their password to update their profile info

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -46,4 +46,13 @@ private
     )
   end
 
+protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
 end
