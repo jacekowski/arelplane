1345868a6be1c3018113706a05a2cc2f675c73a4
Arel English <arelenglish@gmail.com>
2018-04-21 00:54:52 -0400

    fix new comments getting appended to all stories

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('#new_comment').on("ajax:success", function(event, data, status, xhr) {
-    $('#comment_text').val('')
+  $('.new_comment').on("ajax:success", function(event, data, status, xhr) {
+    $('.comment_text').val('')
   }).on('ajax:error', function(event) {
     $('.comments').append("<p>ERROR</p>");
   });
