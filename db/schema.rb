b9467e77c29b42ed50b722213025a8a32db8ce55
Arel English <arelenglish@gmail.com>
2018-04-30 18:28:59 -0400

    add image to card if it exists.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420030724) do
+ActiveRecord::Schema.define(version: 20180430222010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20180420030724) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "map_image_url"
     t.index ["user_id"], name: "index_stories_on_user_id"
   end
 
