1dd729b0cbdabc3c3643dd3518177581d38a4d1d
Arel English <arelenglish@gmail.com>
2018-05-02 12:41:35 -0400

    showing and hiding comments sections works now

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
   }).on('ajax:error', function(event) {
     $('.comments').append("<p>ERROR</p>");
   });
+
+  $('.comments').hide();
+  $('button.comment_button').click(function() {
+    var commentsSection = $(this).closest('.social_actions').nextAll(".comments")
+    if (commentsSection.is(":hidden")) {
+      commentsSection.show();
+    } else {
+      commentsSection.hide();
+    }
+  });
 });
