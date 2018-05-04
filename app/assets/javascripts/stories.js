296a788005bca24cac49be5175db0db036ae2dc3
Arel English <arelenglish@gmail.com>
2018-05-04 08:43:37 -0400

    add flights and ratings are now mutually exclusive

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
 
   $('a.add_flight_button').click(function() {
     $(".story_flight").slideToggle();
+    $('.story_ratings').slideUp();
   });
 
   $('a.add_ratings_button').click(function() {
     $(".story_ratings").slideToggle();
+    $('.story_flight').slideUp();
   });
 });
