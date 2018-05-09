2095ec54052709c1bbfd4a992f2a8ce1984da08b
Arel English <arelenglish@gmail.com>
2018-05-09 00:44:54 -0400

    switch to 10 stories per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def home
     @user = current_user
-    @feed = Story.all.page(params[:page]).per(5)
+    @feed = Story.all.page(params[:page]).per(10)
     @followers = current_user.followers
     @following = current_user.following
 
