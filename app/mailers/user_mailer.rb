60e19105fcb2f965d8cc4b3e4159b65763b76e60
Arel English <arelenglish@gmail.com>
2018-05-16 10:57:54 -0400

    compose all emails. Move story mailers into a story_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,40 +16,7 @@ class UserMailer < ApplicationMailer
     mail(
       to: @following.email,
       reply_to: "no-reply@arelplane.com",
-      subject: "#{@follower.name} followed you!"
-    )
-  end
-
-  def new_comment(commenter, story)
-    @commenter = commenter
-    @story = story
-    @story_owner = story.user
-    mail(
-      to: @story_owner.email,
-      reply_to: "no-reply@arelplane.com",
-      subject: "#{@commenter.name} commented on your post!"
-    )
-  end
-
-  def new_like(liker, story)
-    @liker = liker
-    @story = story
-    @story_owner = story.user
-    mail(
-      to: @story_owner.email,
-      reply_to: "no-reply@arelplane.com",
-      subject: "#{@liker.name} liked your post!"
-    )
-  end
-
-  def following_story_comment(new_commenter, previous_commenter, story)
-    @new_commenter = new_commenter
-    @previous_commenter = previous_commenter
-    @story = story
-    mail(
-      to: @new_commenter.email,
-      reply_to: "no-reply@arelplane.com",
-      subject: "#{@new_commenter.name} also commented on #{@story.user.name}'s post!"
+      subject: "#{@follower.username} followed you!"
     )
   end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -13,7 +13,7 @@ class Story < ApplicationRecord
   has_many :user_ratings
   has_many :ratings, through: :user_ratings, dependent: :destroy
 
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :subscriptions, as: :subscribable, dependent: :destroy
   has_many :subscribers, through: :subscriptions, source: :user
