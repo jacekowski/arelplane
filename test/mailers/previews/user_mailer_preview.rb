43cceee06497b833fb260e63392a42195eb33cbe
Arel English <arelenglish@gmail.com>
2018-05-18 13:51:42 -0400

    Set up the first email test. OMG, it's going to save me so much time!!!

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def welcome_email
+    UserMailer.welcome_email(User.first).deliver_now
+  end
 
+  def new_follower
+    UserMailer.new_follower(User.first, User.last).deliver_now
+  end
 end
