d78aa223cd0ec8d9475ea02dc9324c036d459a4c
Arel English <arelenglish@gmail.com>
2018-06-18 20:39:56 -0400

    switch to a basic payment model for stripe

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,59 +8,56 @@ class PurchasesController < ApplicationController
       source: params[:stripeToken]
     )
 
-    charge = Stripe::Order.create(
-      customer: customer.id,
-      currency: 'usd',
-      items: [
-        {
-          type: 'sku',
-          parent: 'sku_D4c53F5LQqaYuP'
-        }
-      ],
-      shipping: {
-        name: params[:stripeShippingName],
-        address: {
-          line1: params[:stripeShippingAddressLine1],
-          line2: params[:stripeShippingAddressLine2],
-          city: params[:stripeShippingAddressCity],
-          state: params[:stripeShippingAddressState],
-          country: params[:stripeShippingAddressCountry],
-          postal_code: params[:stripeShippingAddressZip]
-        }
-      },
-      email: params[:stripeEmail]
-    )
-
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to craftpad_path
-
     if current_user
       user = current_user
     else
       user = User.find_by(email: params[:stripeEmail])
     end
-    @purchase = Purchase.new(
+    @purchase = Purchase.create(
       user_id: user.try(:id),
       email: params[:stripeEmail],
-      amount: charge["amount"],
-      object: charge["object"],
-      amount_refunded: charge["amount_refunded"],
-      address_city: charge["source"]["address_city"],
-      address_country: charge["source"]["address_country"],
-      address_line1: charge["source"]["address_line1"],
-      address_line2: charge["source"]["address_line2"],
-      address_state: charge["source"]["address_state"],
-      address_zip: charge["source"]["address_zip"],
-      brand: charge["source"]["brand"],
-      country: charge["source"]["country"],
-      description: charge["description"]
+      amount: 1499,
+      address_city: params[:stripeShippingAddressCity],
+      address_country: params[:stripeShippingAddressCountry],
+      address_line1: params[:stripeShippingAddressLine1],
+      address_state: params[:stripeShippingAddressState],
+      address_zip: params[:stripeShippingAddressZip],
+      brand: customer[:cards][:data][0][:brand],
     )
-    if @purchase.save
-      respond_to do |format|
-        format.html { redirect_to @purchase, notice: 'Purchase Successful!' }
-      end
-    end
+
+    charge = Stripe::Charge.create(
+      customer:    customer.id,
+      amount:      @amount,
+      description: 'CRAFTpad Purchase',
+      currency:    'usd'
+    )
+
+    # charge = Stripe::Order.create(
+    #   customer: customer.id,
+    #   currency: 'usd',
+    #   items: [
+    #     {
+    #       type: 'sku',
+    #       parent: 'sku_D4c53F5LQqaYuP'
+    #     }
+    #   ],
+    #   shipping: {
+    #     name: params[:stripeShippingName],
+    #     address: {
+    #       line1: params[:stripeShippingAddressLine1],
+    #       line2: params[:stripeShippingAddressLine2],
+    #       city: params[:stripeShippingAddressCity],
+    #       state: params[:stripeShippingAddressState],
+    #       country: params[:stripeShippingAddressCountry],
+    #       postal_code: params[:stripeShippingAddressZip]
+    #     }
+    #   },
+    #   email: params[:stripeEmail]
+    # )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to craftpad_path
   end
 
   def show
